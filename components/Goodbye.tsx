ca6ed95104966f9fc375344b1c7d9b5dabfe8b36
Faiq Shariff <faiq55@yahoo.com>
2024-11-10 13:40:40 +0500

    <TextReveal text="See ya later!" />

--- components/Goodbye.tsx
+++ components/Goodbye.tsx
@@ -4,7 +4,7 @@ import TextReveal from '@/components/ui/text-reveal';
 const Goodbye: React.FC = () => {
   return (
     <div className="z-10 flex min-h-64 items-center justify-center rounded-lg bg-black">
-      <TextReveal text="See ya later! :) and drop a ⭐ if you like this!" />
+      <TextReveal text="See ya later!" />
     </div>
   );
 };
